3119a98d884cc8c1614cd6339fda3379199455a5
nolantr18 <nolantr18@students.ecu.edu>
2022-04-20 13:35:33 -0400

    Adds username and currency to Character page

--- Final-Project/website/models.py
+++ Final-Project/website/models.py
@@ -1,3 +1,4 @@
+from email.policy import default
 from enum import unique
 from . import db
 from flask_login import UserMixin
@@ -14,5 +15,6 @@ class User(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(15), unique=True)
     password = db.Column(db.String(30))
-    adjective = db.Column(db.Integer, db.ForeignKey('adjective.id'))
-    role = db.Column(db.Integer, db.ForeignKey('role.id'))
+    currency = db.Column(db.Integer, default=0)
+    adjective = db.Column(db.String(30), default='Cowardly')
+    role = db.Column(db.String(30), default='Wizard')
