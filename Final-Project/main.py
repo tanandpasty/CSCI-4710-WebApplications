8515d5a71a24723ac0bf7f0468b54f2f64c6d3cc
boutonc18 <boutonc18@students.ecu.edu>
2022-04-22 15:47:03 -0400

    chat attempt

--- Final-Project/main.py
+++ Final-Project/main.py
@@ -1,7 +1,13 @@
 from website import create_app
 from flask_socketio import SocketIO, send
+
 app = create_app()
 socketio = SocketIO(app)
+
+@socketio.on('message')
+def handleMessage(msg):
+	print('Message: ' + msg)
+	send(msg, broadcast=True)
 
 if __name__ == '__main__':
     socketio.run(app)
