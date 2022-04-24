7b40322d0ccaab80ea3719c83fc1d4db78582330
nolantr18 <nolantr18@students.ecu.edu>
2022-04-23 21:53:22 -0400

    Adds a few comments to try to help out.

--- Final-Project/website/views.py
+++ Final-Project/website/views.py
@@ -1,3 +1,4 @@
+from crypt import methods
 from flask import Blueprint, redirect, render_template, url_for, request
 from flask_login import login_required, login_required, current_user
 from website.models import User
@@ -5,6 +6,12 @@ from . import db
 
 views = Blueprint('views', __name__)
 
+# Below are different lists of adjectives and roles.
+# The html character page will automatically update depending
+# on how much currency the current player possesses
+# adjectives1 are automatically unlocked, adjectives2 are unlocked after
+# reaching a currency of 10, adjective3 are unlocked after
+# reaching a currency of 50. The same goes for roles1-roles3
 adjectives1 = ['Tough', 'Lame', 'Lucky']
 adjectives2 = ['Smart', 'Hot', 'Sneaky']
 adjectives3 = ['Sassy', 'Mean', 'Cocky']
@@ -19,8 +26,6 @@ def home():
 @views.route('/character', methods=['GET', 'POST'])
 @login_required
 def character():
-    user_to_update = User.query.filter_by(username=current_user.username).first()
-    user_to_update.currency = 10
     db.session.commit()
     if request.method == 'POST':
         adjective = request.form.get('adjective')
@@ -28,14 +33,21 @@ def character():
         user_to_update = User.query.filter_by(username=current_user.username).first()
         user_to_update.adjective = adjective
         user_to_update.role = role
-        # user_to_update.currency += 10
         db.session.commit()
         return render_template("character.html", user=current_user, adj1=adjectives1, adj2=adjectives2, adj3=adjectives3, role1=roles1, role2=roles2, role3=roles3)
     else:
         return render_template("character.html", user=current_user, adj1=adjectives1, adj2=adjectives2, adj3=adjectives3, role1=roles1, role2=roles2, role3=roles3)
         
-
-@views.route('/chat')
+@views.route('/chat', methods=['GET', 'POST'])
 @login_required
 def chat():
-    return render_template("chat.html", user=current_user)
+    if request.method == 'POST':
+        # ADD LIKE FUNCTIONALITY HERE
+        # Get the value from the submitted request
+        # Query for the user whose username matches that value
+        # Increment their currency by one
+        # user_to_update = User.query.filter_by(username= WHATEVERTHEVALUEIS).first()
+        # user_to_update.currency = 10
+        pass
+    else:
+        return render_template("chat.html", user=current_user)
