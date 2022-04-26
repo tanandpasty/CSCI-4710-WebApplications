032820477299f786fcf7144a65dcc7312bbc6727
boutonc18 <boutonc18@students.ecu.edu>
2022-04-26 17:16:03 -0400

    updated chat functions

--- Final-Project/main.py
+++ Final-Project/main.py
@@ -6,18 +6,21 @@ app = create_app()
 @socketio.on('user_joined')
 def joined(msg):
 	username = msg['user']
-	emit('status', {'msg': ' has entered the realm' + '>', 'user': username}, broadcast=True)
+	emit('status', {'msg': ' has entered the realm' + '>', 'user': username})
+	emit('status', {'msg': ' has entered the realm' + '>', 'user': username}, broadcast=True, include_self=False)
 
 @socketio.on('message')
 def message(msg):
 	username = msg['user']
-	emit('message', {'msg': msg['msg'], 'user': username}, broadcast=True)
+	emit('message', {'msg': msg['msg'], 'user': username})
+	emit('message', {'msg': msg['msg'], 'user': username}, broadcast=True, include_self=False)
 
 
 @socketio.on('user_left')
 def user_left(msg):
 	username = msg['user']
-	emit('status', {'msg': ' has left the realm' + '>', 'user': username}, broadcast=True)
+	emit('status', {'msg': ' has left the realm' + '>', 'user': username})
+	emit('status', {'msg': ' has left the realm' + '>', 'user': username}, broadcast=True, include_self=False)
 
 
 if __name__ == '__main__':
