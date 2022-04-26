7a5164f15812e99947f2846bb859198e7a86e339
nolantr18 <nolantr18@students.ecu.edu>
2022-04-26 18:27:37 -0400

    Adds functionality to the three dialogue buttons on the home screen. Adjusts the dialogue box location.

--- Final-Project/website/static/index.js
+++ Final-Project/website/static/index.js
@@ -1,5 +1,17 @@
 $(document).ready(function() {
-    
+
+    $('#howAreYouButton').click(function() {
+        $("#homepage-reponse").html("I am sick...");
+    })
+
+    $('#whatIsYourRoleButton').click(function() {
+        $("#homepage-reponse").html("I am Sick wizard_almight09 The Wizard");
+    })
+
+    $('#whatIsRoleChatButton').click(function() {
+        $("#homepage-reponse").html("Role Chat is a place where people act like who they are not.");
+    })
+
     const socket = io();
     socket = io.connect('http://127.0.0.1:5000/');
 
